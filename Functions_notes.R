a8a93ba6c9f6cd1d54fbe6caa2bb9d7af93cdf3b
Andy Tu <andytu@mit.edu>
2020-06-11 10:47:15 -0400

    Update Functions_notes.R

--- Functions_notes.R
+++ Functions_notes.R
@@ -1,7 +1,17 @@
-##Source scirpt for commonly used functions, presets, and other notes for general R programming.
+##Source script for commonly used functions, presets, and other notes for general R programming.
 
 install_if_missing <- function(packages) {
   if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
     install.packages(setdiff(packages, rownames(installed.packages())))
   }
 }
+
+Axis_themes <- theme(plot.title = element_text(size = 8),
+                     axis.title = element_text(size = 8), 
+                     axis.text = element_text(size = 6),
+                     axis.text.x = element_text(size = 6),
+                     legend.text = element_text(size =6),
+                     legend.title = element_text(size = 8),
+                     strip.text.x = element_text(size = 8))
+
+#theme(axis.text.x = element_text(angle = 90, hjust = 1))
