95bac9eca4f4b3210bfba6d067dc9723727f67d8
MiTPenguin <angtuster@gmail.com>
2020-06-17 13:09:05 -0400

    Update find_pcCut

--- Functions_notes.R
+++ Functions_notes.R
@@ -15,3 +15,27 @@ Axis_themes <- theme(plot.title = element_text(size = 8),
                      strip.text.x = element_text(size = 8))
 
 #theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+find_pcCut <- function(seurat){
+  #pull out the pca deviation from each PCA components
+  PCA_dev <- seurat@reductions$pca@stdev
+  #This part is doing some geometry calculation. we should write this into a function.
+  allCoor<-cbind(seq_along(PCA_dev),PCA_dev)
+  line_Vec<-allCoor[length(PCA_dev),]-allCoor[1,]
+  ## normalize the line vector
+  lineVecN = line_Vec / sqrt(sum(line_Vec^2));
+  ## find the distance from each point to the line:
+  ## vector between all points and first point
+  vecFromFirst<-allCoor-do.call("rbind", rep(list(allCoor[1,]), length(PCA_dev)))
+  q<-do.call("rbind", rep(list(lineVecN), length(PCA_dev)))
+  scalarProduct<-q[,1]
+  for (i in 1:length(PCA_dev)) {
+    scalarProduct[i]<- vecFromFirst[i,] %*% q[i,]
+  }
+  vecFromFirstParallel = scalarProduct * q
+  vecToLine = vecFromFirst - vecFromFirstParallel
+  distToLine = sqrt(rowSums(vecToLine^2))
+  ##Point in elbow is point furthest from line connecting first and last point
+  pcCut<-which.max(distToLine) 
+  return(pcCut)
+}
